86b1cfadf62a3f97edf87af4b7c9091316dc73e1
Omar <omohammed7869@gmail.com>
2021-01-01 12:58:00 -0600

    reduced from O(n^2) -> O(n) solution

--- src/solution/MoveZeroes.java
+++ src/solution/MoveZeroes.java
@@ -8,7 +8,7 @@ public class MoveZeroes {
 		obj.moveZeroes(new int[] {0, 1, 0, 3, 12});
 	}
 	
-	public void moveZeroes(int[] nums) {
+	public void moveZeroesv2(int[] nums) {
 		int numOfZeroes = this.findZeroes(nums);
 		int ctr = 0;
 		
@@ -35,4 +35,21 @@ public class MoveZeroes {
 		}
 		return ctr;
 	}
+
+	public void moveZeroes(int[] nums) { 
+		int index = 0;
+		for(int i = 0; i < nums.length; i++) { 
+			if(nums[i] != 0) {
+				nums[index++] = nums[i];
+			}
+		}
+		
+		for(; index < nums.length; index++) {
+			nums[index] = 0;
+		}
+		
+		System.out.println(Arrays.toString(nums));
+	}
 }
+
+
