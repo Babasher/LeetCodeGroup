4cdae7681056fb84339bb54769d987dc832924cc
Omar <omohammed7869@gmail.com>
2021-01-03 02:25:28 -0600

    solved it

--- src/solution/PathSum.java
+++ src/solution/PathSum.java
@@ -3,39 +3,28 @@ package solution;
 public class PathSum {
 	public static void main(String[] args) {
 		PathSum obj = new PathSum();
-		TreeNode root = new TreeNode(4);
-		root.left = new TreeNode(1);
-		root.right = new TreeNode(2);
+		TreeNode root = new TreeNode(5);
+		root.left = new TreeNode(3);
+		root.right = new TreeNode(4);
 		
-		System.out.println(obj.hasPathSum(root, 6));
+		System.out.println(obj.hasPathSum(root, 9));
 		
 	}
 	
 	public boolean hasPathSum(TreeNode root, int sum) {
-		int result = this.findPathSum(root, 0, sum);
-		System.out.println(result);
-		return result == sum;
+		if(root == null) {
+			return false;
+		}
+		
+		if(root.val == sum && root.left == null && root.right == null) { //ensures were on a leaf and path exists
+			return true;
+		}
+		
+		boolean left = hasPathSum(root.left, sum - root.val);
+		boolean right = hasPathSum(root.right, sum - root.val);
+		
+		return left || right;
 	}
 	
-	public int findPathSum(TreeNode root, int accu, int sum) {
-		System.out.println(accu);
-		
-		if(root == null) {
-			return accu;
-		}
-		
-		accu += root.val;
-		findPathSum(root.left, accu, sum);
-		findPathSum(root.right, accu, sum);
-		
-		if(accu == sum && root.left == null && root.right == null) {
-			System.out.println("Hello");
-			return accu;
-		}
-		
-		if(accu == sum) return accu;
-		return 0;
-		
-	}
 
 }
