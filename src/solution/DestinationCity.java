f940b1873414dd14c42a36d22aabb724ccf5ca68
Omar Mohammed <omohammed7869@gmail.com>
2021-01-07 19:32:23 -0600

    Cleaned up the file

--- src/solution/DestinationCity.java
+++ src/solution/DestinationCity.java
@@ -8,31 +8,16 @@ import java.util.List;
 public class DestinationCity {
 	public static void main(String[] args) {
 		DestinationCity obj = new DestinationCity();
-		List<List<String>> cities = new ArrayList<>();
-//		List<String> c1 = new ArrayList<>();
-//		c1.add("B");
-//		c1.add("C");
-//		List<String> c2 = new ArrayList<>();
-//		c2.add("D");
-//		c2.add("B");
-//		List<String> c3 = new ArrayList<>();
-//		c3.add("C");
-//		c3.add("A");
+		List<List<String>> cities = new ArrayList<>();		
 		
 		List<String> p = new ArrayList<>();
 		p.add("A");
 		p.add("Z");
 		cities.add(p);
 		
-		
-//		cities.add(c1);
-//		cities.add(c2);
-//		cities.add(c3);
-		
 		String result = obj.destCity(cities);
 		
 		System.out.println(result);
-		
 	}
 	
 	 public String destCity(List<List<String>> paths) {
@@ -42,11 +27,7 @@ public class DestinationCity {
 			 hasOutbound.add(city.get(0));
 			 inBound.add(city.get(1));
 		 }
-		 
-//		 System.out.println(hasOutbound.toString());
-//		 System.out.println(inBound.toString());
-		 
-		 
+
 		 for(String s : inBound) {
 			 if(!hasOutbound.contains(s)) {
 				 return s;
