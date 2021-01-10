6d9c5d4a451e84e0d5fd7b38a81dd82d06204758
Omar Mohammed <omohammed7869@gmail.com>
2021-01-10 13:38:18 -0600

    solved, works on leet

--- src/solution/MiddleOfLinkedList.java
+++ src/solution/MiddleOfLinkedList.java
@@ -4,11 +4,11 @@ public class MiddleOfLinkedList {
 	public static void main(String[] args) {
 		MiddleOfLinkedList obj = new MiddleOfLinkedList();
 		ListNode head = new ListNode(1);
-//		head.next = new ListNode(2);
-//		head.next.next = new ListNode(3);
-//		head.next.next.next = new ListNode(4);
-//		head.next.next.next.next = new ListNode(5);
-//		head.next.next.next.next.next = new ListNode(6);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(5);
+		head.next.next.next.next.next = new ListNode(6);
 		
 		System.out.println(obj.middleNode(head).val);
 	}
